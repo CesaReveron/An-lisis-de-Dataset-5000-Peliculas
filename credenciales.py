21db51ca5bceff1f8b5cbcefb382470a2893a5dc
CesaReveron <reveronprograma@gmail.com>
2026-05-18 20:10:17 +0200

    Carga de los CSV y reestructuracion de crednciales
    Pasamos credenciales.py al notebook correcto ya que estaba fuera del mismo y cargamos los csv en TiDB

---
+++ credenciales.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+mysql_config = {
+    "host": "gateway01.eu-central-1.prod.aws.tidbcloud.com",
+    "port": 4000,
+    "user": "2UKg9SGLbaBimU1.root",
+    "password": "lFniGMm9rem5OvbV",
+    "database": "proyecto_final",
+    "ssl_disabled": False,
+}
